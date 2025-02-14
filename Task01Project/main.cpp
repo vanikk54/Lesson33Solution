d9057efbf6b1549315f620f53bfbc2b3f1fd7ca1
vanikk54 <skugoli@gmail.com>
2025-02-14 19:08:38 +0300

    Add project files.

---
+++ Task01Project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "testing..." << endl;
+	cout << "testing..." << endl;
+	cout << "testing..." << endl;
+	cout << "testing..." << endl;
+	cout << "testing..." << endl;
+	return 0;
+}
